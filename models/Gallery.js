bfd1ff784458e573e8c330ec2197de0bf2d035c4
Jess Greene <jess.greene9@gmail.com>
2021-06-28 18:16:35 -0400

    added gallery table and gallery seeds for images for each seeded trail

---
+++ models/Gallery.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Gallery extends Model {}
+
+
+  Gallery.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+    image_name: { 
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    
+    url:{
+        type: DataTypes.STRING,
+        allowNull:false,
+  },
+  trail_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "Trail",
+      key: "id",
+    },
+   },
+}, 
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Gallery',
+  });
+  
+  module.exports = Gallery;
