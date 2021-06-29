4814c66f46b0c9e029b57fb9c793f711cff0f5f7
Alexandra Hurst <82131699+AlNHurst@users.noreply.github.com>
2021-06-29 16:03:25 -0400

    added handlebar for indiv. trails and added location to each render

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -6,54 +6,62 @@ router.get('/', async (req, res) => {
     const getLocationData = await Location.findAll();
     const locations = getLocationData.map((location) => location.get({ plain: true }));
 
-      const trailData = await Trail.findAll({
-        order: ['distance'],
-        include: [Location, Gallery],
-        limit: 4
-      });
-      const trails = trailData.map((trail) => {
-        return trail.get({ plain: true });
-      }) 
-      
-      res.render('homepage', { trails, locations });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-  
-  
-  router.get('/search/:id', async (req, res) => {
-    try {
-      const locationData = await Location.findByPk(req.params.id, {
-        include: [{
-          model:Trail,
-          include: [Gallery]
-        }],
-      });
-      console.log(locationData);
-      const locations = JSON.parse(JSON.stringify(locationData));
+    const trailData = await Trail.findAll({
+      order: ['distance'],
+      include: [Location, Gallery],
+      limit: 4
+    });
+    const trails = trailData.map((trail) => {
+      return trail.get({ plain: true });
+    })
 
-      res.render('search', {locations});
-    } catch (err) {
+    res.render('homepage', { trails, locations });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+router.get('/search/:id', async (req, res) => {
+  try {
+    const locationData = await Location.findByPk(req.params.id, {
+      include: [{
+        model: Trail,
+        include: [Gallery]
+      }],
+    });
+    console.log(locationData);
+    const locations = JSON.parse(JSON.stringify(locationData));
+
+    res.render('search', { locations });
+  } catch (err) {
     res.status(500).json(err)
   };
 });
 
 router.get('/trailsgallery', async (req, res) => {
-  res.render('trailsgallery')
+  const getLocationData = await Location.findAll();
+  const locations = getLocationData.map((location) => location.get({ plain: true }));
+  res.render('trailsgallery', { locations })
 });
 
 router.get('/trail', async (req, res) => {
-  res.render('trail')
+  const getLocationData = await Location.findAll();
+  const locations = getLocationData.map((location) => location.get({ plain: true }));
+  res.render('trail', { locations })
 });
 
 router.get('/login', async (req, res) => {
-  res.render('login')
+  const getLocationData = await Location.findAll();
+  const locations = getLocationData.map((location) => location.get({ plain: true }));
+  res.render('login', { locations })
 });
 
 router.get('/profile', async (req, res) => {
-  res.render('profile')
+  const getLocationData = await Location.findAll();
+  const locations = getLocationData.map((location) => location.get({ plain: true }));
+  res.render('profile', { locations })
 });
 
 
